fd3cb90bac80fabe1c566ce2a1ce300c17bd5ae9
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-09 23:06:05 +0100

    adds addbook method to library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,19 @@
-public class Library {
+import java.util.ArrayList;
 
+public class Library {
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.stock = new ArrayList<>();
+    }
+
+    public int bookCount() {
+        return stock.size();
+    }
+
+    public void addBook(Book book) {
+        stock.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Library library;
+    Book book;
+
+    @Before
+    public void beforeEach(){
+        library = new Library(50);
+    }
+
+    @Test
+    public void bookCount(){
+        assertEquals(0, library.bookCount() );
+    }
+
+    @Test
+    public void addBook(){
+        library.addBook(book);
+        assertEquals(1, library.bookCount() );
+    }
 }
