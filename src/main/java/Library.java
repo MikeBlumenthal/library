15daed0085eddb14e844763b01a7d9b3fed29aad
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-10 00:26:54 +0100

    finishes advanced extension

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> stock;
     private int capacity;
+    private HashMap<String,Integer> genreCount;
 
     public Library(int capacity){
         this.capacity = capacity;
         this.stock = new ArrayList<>();
+        this.genreCount = new HashMap<>();
     }
 
     public int bookCount() {
@@ -21,4 +24,18 @@ public class Library {
         return bookCount()>=capacity;
     }
 
+    public void genreCount(){
+        for (Book book:stock) {
+            String genre = book.getGenre();
+            if (!genreCount.containsKey(genre) ){
+                genreCount.put(genre, 1);
+            }else{
+                genreCount.put(genre, genreCount.get(genre)+1);
+            }
+        }
+    }
+
+    public HashMap<String, Integer> getGenreCount() {
+        return genreCount;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,15 +1,22 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     Library library;
     Book book;
+    Book book2;
+    Book book3;
 
     @Before
     public void beforeEach(){
-        library = new Library(50);
+        library = new Library(3);
+        book = new Book("Moby Dick","Herman Melville","Fiction");
+        book2 = new Book("Death At Seaworld","David Kirby","Non-fiction");
+        book3 = new Book("Jaws","Peter Benchley","Fiction");
     }
 
     @Test
@@ -24,7 +31,26 @@ public class LibraryTest {
     }
 
     @Test
-    public void libraryFull(){
+    public void libraryNotFull(){
         assertEquals(false, library.checkIsFull() );
     }
+
+    @Test
+    public void libraryFull(){
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(true, library.checkIsFull());
+    }
+
+    @Test
+    public void canGenreCount(){
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(3, library.bookCount() );
+        library.genreCount();
+        assertEquals(2, library.getGenreCount().size() );
+    }
+
 }
